bb79092d853c4e2f2f95bad4aa0e4c684b854e0e
Jesús Fernández <jesusfer@microsoft.com>
2022-06-08 16:43:36 +0200

    Use dev npm

--- deploy.sh
+++ deploy.sh
@@ -113,7 +113,7 @@ selectNodeVersion
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running npm install"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install
   echo "Running gulp build"
   eval ./node_modules/.bin/gulp build
   exitWithMessageOnError "gulp failed"
